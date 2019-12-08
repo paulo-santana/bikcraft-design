942a03a7590b925370a49daa66d5e30d20edb937
Paulo Santana <paulo.santana.r@gmail.com>
2019-12-08 02:10:46 -0300

    feat(mail): add mail sending support for contact and products pages

--- 04-dev/js/script.js
+++ 04-dev/js/script.js
@@ -14,3 +14,14 @@ if (window.SimpleSlide) {
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+if (window.SimpleForm) {
+  new SimpleForm({
+    form: ".form-php",
+    button: "#enviar",
+    erro:
+      '<div id="form-erro"><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para contato@bikcraft.com</p></div>',
+    sucesso:
+      '<div id="form-sucesso"><h2>Formulário enviado com sucesso!</h2><p>Em breve entraremos em contato com você.</p></div>'
+  });
+}
